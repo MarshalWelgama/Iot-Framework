3f802e9a2a5724a80eee22ac890d80d60f466097
Marshal Welgama <marshalwelgama@gmail.com>
2021-08-08 23:14:37 +1000

    new: mqtt added on each node

--- edge.js
+++ edge.js
@@ -1,1 +1,26 @@
-console.log('from edge')
+const config = require('./config.json')
+var mqtt = require('mqtt')
+if (config.mqttLocation) { //connect to mqqt broker if already setup
+    var client = mqtt.connect(`mqtt://${config.mqttLocation}`)
+    try {
+        client.on('connect', function () {
+            console.log('Mqtt broker connected')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// client.on('message', function (topic, message) {
+//     // prints out message when received from subscribe
+//     console.log(message.toString())
+
+// })
+
+// client.subscribe('presence')
+
+// client.publish('presence', 'Hello mqtt') //publish messages, topic/message
+
+//edge server will always be on standby, make a way for it to identify which image it is going to be looking out for. 
+//therefore we will need to alter some of the questions that we are asking for edge server
+//maybe do the topic as name+imagename so we know its specific
--- iot.js
+++ iot.js
@@ -1,5 +1,48 @@
 const config = require('./config.json')
-const InocrrectSetup = config.registerySetup === false
-if (InocrrectSetup) {
+
+var mqtt = require('mqtt')
+
+if (config.registerySetup == false) {
     console.log('Incorrect setup, please setup registery on edge server first')
+    process.exit()
+}
+
+var client = mqtt.connect(`mqtt://${config.mqttLocation}`) //connect to mqtt broker
+try {
+    client.on('connect', function () {
+        console.log('Mqtt broker connected')
+    })
+} catch (error) {
+    console.log(error)
+}
+
+
+
+
+client.subscribe(`${config.name}`)
+
+client.publish(`${config.name}`, 'Hello mqtt') //publish messages, topic/message
+
+client.on('message', function (topic, message) {
+    // prints out message when received from subscribe
+    console.log(message.toString())
+    console.log(topic.toString())
+})
+
+function AddImage(imageLoc) {
+    //add image to the registery
+    return null
+}
+function RunImage() {
+    //run docker image on the node
+    return null
+}
+function StopImage() {
+    //stop image from running 
+    return null
+}
+function updateNode() {
+    // this is where we can assess how much the docker image is using, set certain threshold and send mqtt message
+    // include stop image function here
+    return null
 }
