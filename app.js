c070a439f74889a5df9a6b687305bc5475508d6f
Marshal Welgama <marshalwelgama@gmail.com>
2021-08-11 13:35:26 +1000

    new: AddImage function created

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var RegisteryLocation;
+var RegistryLocation;
 var Name;
 var NodeType;
 /**
@@ -6,13 +6,17 @@ var NodeType;
  */
 
 'use strict';
-const fs = require('fs');
+// const fs = require('fs');
 const inquirer = require('inquirer');
-const cp = require('child_process')
+const iot = require('./iot.js');
+const edge = require('./edge.js');
+const cloud = require('./cloud.js');
+var configuration;
+
 const initialize = [
     {
         type: 'confirm',
-        name: 'registerySetup',
+        name: 'registrySetup',
         message: "Has docker registery been setup?",
         default() {
             return false;
@@ -21,9 +25,9 @@ const initialize = [
     },
     {
         type: 'input',
-        name: 'registeryLocation',
+        name: 'registryLocation',
         message: "Docker registery location? (192.168.x.x:xxxx)",
-        when: (answers) => answers.registerySetup === true,
+        when: (answers) => answers.registrySetup === true,
 
     },
     {
@@ -50,7 +54,7 @@ const initialize = [
         type: 'confirm',
         name: 'registryImage',
         message: "Docker image hosted in registery?",
-        when: (answers) => answers.registerySetup === true,
+        when: (answers) => answers.registrySetup === true && answers.nodeType == 'iot',
         default() {
             return true;
         },
@@ -60,46 +64,43 @@ const initialize = [
         type: 'input',
         name: 'imageLocation',
         message: "Image location:",
-        when: (answers) => answers.registryImage === false || (answers.nodeType === 'iot' && answers.registerySetup === false),
+        when: (answers) => answers.nodeType == 'iot' && answers.registryImage === false,
     },
     {
-        type: 'confirm',
-        name: 'mqttSetup',
-        message: "Do you want to setup MQTT Broker?",
-        when: (answers) => answers.nodeType === 'edge' || answers.nodeType === 'cloud',
-        default() {
-            return true
-        }
+        type: 'input',
+        name: 'imageName',
+        message: "Name of docker image:",
+        when: (answers) => answers.nodeType == 'iot',
     },
     {
         type: 'input',
         name: 'mqttLocation',
-        message: "Mqtt broker address:",
-        when: (answers) => answers.mqttSetup === false || answers.nodeType === 'iot',
+        message: "Mqtt broker address:"
     },
 
 ]
 
 inquirer.prompt(initialize).then((answers) => {
-    RegisteryLocation = answers.registeryLocation
+    RegistryLocation = answers.registryLocation
     Name = answers.name
     NodeType = answers.nodeType
     console.log(JSON.stringify(answers));
-    fs.writeFile("config.json", JSON.stringify(answers), function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
+    // fs.writeFile("config.json", JSON.stringify(answers), function (err) {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    // });
+    configuration = answers
 }).then(() => {
     switch (NodeType) {
         case 'iot':
-            cp.fork('iot.js')
+            iot.run(configuration)
             break;
         case 'edge':
-            cp.fork('edge.js')
+            edge.run(configuration)
             break;
         case 'cloud':
-            cp.fork('cloud.js')
+            cloud.run(configuration)
             break;
         default:
             break;
--- iot.js
+++ iot.js
@@ -1,37 +1,19 @@
-const config = require('./config.json')
-
+const { builtinModules } = require('module')
+var shell = require('./shellHelp.js')
+//const config = require('./config.json')
 var mqtt = require('mqtt')
 
-if (config.registerySetup == false) {
-    console.log('Incorrect setup, please setup registery on edge server first')
-    process.exit()
-}
-
-var client = mqtt.connect(`mqtt://${config.mqttLocation}`) //connect to mqtt broker
-try {
-    client.on('connect', function () {
-        console.log('Mqtt broker connected')
-    })
-} catch (error) {
-    console.log(error)
-}
 
 
 
-
-client.subscribe(`${config.name}`)
-
-client.publish(`${config.name}`, 'Hello mqtt') //publish messages, topic/message
-
-client.on('message', function (topic, message) {
-    // prints out message when received from subscribe
-    console.log(message.toString())
-    console.log(topic.toString())
-})
-
-function AddImage(imageLoc) {
-    //add image to the registery
-    return null
+function AddImage(iL, rL, iN) {
+    var commands = [
+        `docker load -i ${iL}`, //load image to our docker images
+        `docker tag ${iN} ${rL}/${iN}`, //prepare to be pushed to registry
+        `docker push ${rL}/${iN}`, //push to registry
+    ]
+    console.log('Adding image to registry please wait..')
+    shell.exec_commands(commands)
 }
 function RunImage() {
     //run docker image on the node
@@ -43,6 +25,41 @@ function StopImage() {
 }
 function updateNode() {
     // this is where we can assess how much the docker image is using, set certain threshold and send mqtt message
+    // send mqtt message to higher up
     // include stop image function here
     return null
 }
+
+function run(config) {
+    console.log(config)
+    if (config.registrySetup == false) {
+        console.log('Incorrect setup, please setup registery on edge server first')
+        process.exit()
+    }
+    var client = mqtt.connect(`mqtt://${config.mqttLocation}`) //connect to mqtt broker
+    try {
+        client.on('connect', function () {
+            console.log('Mqtt broker connected')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    AddImage(config.imageLocation, config.registryLocation, config.imageName)
+    //client.subscribe(`${config.name}`)
+
+    //client.publish('Resource-Pool', 'Hello mqtt') //publish messages, topic/message
+
+    // client.on('message', function (topic, message) {
+    //     // prints out message when received from subscribe
+    //     console.log(message.toString())
+    //     console.log(topic.toString())
+    // })
+    console.log('wtf')
+    // start doing shit
+}
+
+module.exports = {
+    run
+};
+
+
